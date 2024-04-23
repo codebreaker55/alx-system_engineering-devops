82dc20e1b5a5828f2925a2cbbde2074f01d88d2a
belal <belalismail252@gmail.com>
2024-04-23 01:49:52 -0700

    puppet issue

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-#!/usr/bin/puppet
-# Using Puppet to install flask from pip3 version 2.1.0
+# Puppet manifest to install flask from pip3 version 2.1.0
+
+# install flask package with version 2.1.0 using pip3
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip',
+  provider => 'pip3',
 }
