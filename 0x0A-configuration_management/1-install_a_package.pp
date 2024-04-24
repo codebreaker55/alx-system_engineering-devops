ea859021138c6366f50e98c65f2e94d4c4141e99
belal <belalismail252@gmail.com>
2024-04-24 14:53:42 -0700

    puppet flusk fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,13 @@
 # Puppet manifest to install flask from pip3 version 2.1.0
 
+# install install version 2.0.2 of werkzeug using pip3
+package { 'werkzeug':
+  ensure   => '2.0.2',
+  provider => 'pip3',
+}
+
 # install flask package with version 2.1.0 using pip3
-package {'flask':
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
