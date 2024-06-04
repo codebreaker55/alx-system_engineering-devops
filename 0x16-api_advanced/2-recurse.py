f20d2fb4ee4d81d06aa21d34245192b74697a90e
belal <belalismail252@gmail.com>
2024-06-04 14:21:31 +0300

    new attemp

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,31 +4,42 @@ a recursive function that queries the Reddit API,
 returns a list containing the titles of all hot articles for a given subreddit
 """
 import requests
+import sys
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
+def title_add(hot_list, hot_posts):
+    """ adds an item into the list """
+    if len(hot_posts) == 0:
+        return
+    hot_list.append(hot_posts[0]['data']['title'])
+    hot_posts.pop(0)
+    title_add(hot_list, hot_posts)
+
+
+def recurse(subreddit, hot_list=[], after=None):
     """return titles of all of the hot articles, if no results return none"""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    usr_agent = 'Mozilla/5.0'
     headers = {
-        "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)"
+        'User-Agent': usr_agent
     }
+
     params = {
-        "after": after,
-        "count": count,
-        "limit": 100
+        'after': after
     }
-    resp = requests.get(url, headers=headers, params=params,
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    resp = requests.get(url,
+                        headers=headers,
+                        params=params,
                         allow_redirects=False)
-    if resp.status_code == 404:
+
+    if resp.status_code != 200:
         return None
 
-    res = resp.json().get("data")
-    after = res.get("after")
-    count += res.get("dist")
-    for ch in res.get("children"):
-        hot_list.append(ch.get("data").get("title"))
-
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
+    dic = resp.json()
+    hot_posts = dic['data']['children']
+    title_add(hot_list, hot_posts)
+    after = dic['data']['after']
+    if not after:
+        return hot_list
+    return recurse(subreddit, hot_list=hot_list, after=after)
