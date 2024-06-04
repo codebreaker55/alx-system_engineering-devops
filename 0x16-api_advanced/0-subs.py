c4dc6959cac5d88ef2d6a4702043c9a202059c55
belal <belalismail252@gmail.com>
2024-06-04 13:32:48 +0300

    new try

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,17 +8,18 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    returns the number of subscribers, If not a valid subreddit, return 0
+     returns the number of subscribers, If not a valid subreddit, return 0
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    # getting user agent
-    # using Mozilla/5.0 to identify the browser or client making the request
-    header = {"User-Agent": "Mozilla/5.0"}
-    # get request to the API endpoint using requests.get()
-    resp = requests.get(url, headers=header, allow_redirects=False)
-    if resp.status_code == 200:
-        data = resp.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+
+    # get user agent
+    # https://stackoverflow.com/questions/10606133/ -->
+    # sending-user-agent-using-requests-library-in-python
+    headers = requests.utils.default_headers()
+    headers.update({'User-Agent': 'My User Agent 1.0'})
+
+    resp = requests.get(url, headers=headers).json()
+    subscribers = resp.get('data', {}).get('subscribers')
+    if not subscribers:
         return 0
+    return subscribers
