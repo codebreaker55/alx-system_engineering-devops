cce5df3738e9c7af2b65f29610aaa03635b87f23
belal <belalismail252@gmail.com>
2024-06-04 14:08:10 +0300

    Recurse it!

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+a recursive function that queries the Reddit API,
+and returns a list containing the titles of all hot articles for a given subreddit
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """return titles of all of the hot articles, if no results return none"""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    resp = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if resp.status_code == 404:
+        return None
+
+    res = resp.json().get("data")
+    after = res.get("after")
+    count += res.get("dist")
+    for ch in res.get("children"):
+        hot_list.append(ch.get("data").get("title"))
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
