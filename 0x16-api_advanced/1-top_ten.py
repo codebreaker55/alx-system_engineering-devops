a520998a748353b866c9af7f7f2fe7354d1b6be1
belal <belalismail252@gmail.com>
2024-06-06 11:47:48 +0300

    top 10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -15,8 +15,10 @@ def top_ten(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/hot/.json?limit=10'
     resp = requests.get(url, headers=header, allow_redirects=False)
 
-    try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
-    except Exception:
+    if resp.status_code == 200:
+        data = resp.json()
+        posts = data['data']['children']
+        for post in posts:
+            print(post['data']['title'])
+    else:
         print(None)
