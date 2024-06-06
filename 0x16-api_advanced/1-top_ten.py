2ef7540e2e02025fdaef929a78737a1b02575d7a
belal <belalismail252@gmail.com>
2024-06-06 11:41:04 +0300

    top 10 task

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,7 +4,6 @@ a function that queries the Reddit API and prints the titles,
 of the first 10 hot posts listed for a given subreddit
 """
 import requests
-import sys
 
 
 def top_ten(subreddit):
@@ -12,21 +11,14 @@ def top_ten(subreddit):
     queries the Reddit API and prints the titles of the first 10 hot posts,
     If not a valid subreddit, print None
     """
-    user = {'User-Agent': 'Mozilla/5.0'}
+    header = {'User-Agent': 'Mozilla/5.0'}
     url = f'https://www.reddit.com/r/{subreddit}/hot/.json?limit=10'
+    resp = requests.get(url, headers=header, allow_redirects=False)
 
-    try:
-        response = requests.get(url, headers=user, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            for post in data.get('data', {}).get('children', []):
-                print(post.get('data', {}).get('title', ''))
-    except Exception as e:
-        print(None)
-
-
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        top_ten(sys.argv[1])
+    if resp.status_code == 200:
+        data = resp.json()
+        posts = data['data']['children']
+        for post in posts:
+            print(post['data']['title'])
     else:
-        print("Please provide a subreddit as a command-line argument.")
+        print (None)
